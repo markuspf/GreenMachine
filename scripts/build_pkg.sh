439bebc120245562ad6a34650bc21b20a44a9368
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-10-18 12:01:12 +0100

    Add standard testing setup

---
+++ scripts/build_pkg.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -ex
+
+# ensure coverage is turned on
+export CFLAGS="$CFLAGS -fprofile-arcs -ftest-coverage"
+export LDFLAGS="$LDFLAGS -fprofile-arcs"
+
+if [[ $ABI = 32 ]]
+then
+    export CFLAGS="$CFLAGS -m32"
+    export LDFLAGS="$LDFLAGS -m32"
+fi
+
+# Currently this package has no compiled code
+# build this package
+# ./autogen.sh
+# ./configure --with-gaproot=$GAPROOT
+# make -j4 V=1
+
+# ... and link it into GAP pkg dir
+ls
+ls $GAPROOT
+ls $GAPROOT/pkg
+ln -s $PWD $GAPROOT/pkg/
